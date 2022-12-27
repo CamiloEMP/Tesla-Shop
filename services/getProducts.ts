11d4b03b1204d26a61a35dd928c5c824b19f66b8
b7c8p <morapaezc@gmail.com>
2022-12-27 11:33:00 -0500

    add gender condition

--- services/getProducts.ts
+++ services/getProducts.ts
@@ -3,10 +3,21 @@ import { NextApiRequest, NextApiResponse } from 'next'
 import { db } from 'db/db'
 import { ProductModel } from 'models/Product'
 import { DataProduct } from 'pages/api/products'
+import { SHOP_CONSTANTS } from 'db/constants'
 
 export const getProducts = async (req: NextApiRequest, res: NextApiResponse<DataProduct>) => {
+  const { gender = 'all' } = req.query
+
+  let condition = {}
+
+  if (gender !== 'all' && SHOP_CONSTANTS.validGenders.includes(`${gender}`)) {
+    condition = { gender }
+  }
+
   await db.connect()
-  const products = await ProductModel.find().select('title images price inStock slug -_id').lean()
+  const products = await ProductModel.find(condition)
+    .select('title images price inStock slug -_id')
+    .lean()
 
   await db.disconnect()
 
