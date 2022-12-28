840361c2b5e645fbdc5ebc7c217860e27563133c
b7c8p <morapaezc@gmail.com>
2022-12-28 18:15:57 -0500

    change prop Component

--- components/products/ProductList.tsx
+++ components/products/ProductList.tsx
@@ -3,15 +3,21 @@ import { Product } from 'interfaces/products'
 import { ProductCard } from './ProductCard'
 
 interface Props {
-  products: Product[]
+  products: Product[] | undefined
 }
 
 export const ProductList = ({ products }: Props) => {
   return (
     <section className="grid grid-cols-1 justify-items-center sm:justify-start sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
-      {products.map((product) => (
-        <ProductCard {...product} key={product.slug} />
-      ))}
+      {products ? (
+        <>
+          {products.map((product) => (
+            <ProductCard {...product} key={product.slug} />
+          ))}
+        </>
+      ) : (
+        <h1>No encontramos lo que estas busncando</h1>
+      )}
     </section>
   )
 }
