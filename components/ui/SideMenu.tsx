a08bd38cbd1802ecd80c3d9fb8cc56557b29b6b0
b7c8p <morapaezc@gmail.com>
2022-12-28 18:14:37 -0500

    update style sidemenu and add search

--- components/ui/SideMenu.tsx
+++ components/ui/SideMenu.tsx
@@ -1,6 +1,12 @@
+import { useEffect, useState } from 'react'
+import { createPortal } from 'react-dom'
+import { Inter } from '@next/font/google'
+import { useRouter } from 'next/router'
 import Link from 'next/link'
 
 import { useUI } from 'hooks/useUI'
+
+const inter = Inter({ subsets: ['latin'], variable: '--font-inter' })
 
 import {
   ChildrenIcon,
@@ -14,66 +20,98 @@ import {
 } from './icons'
 import { CloseIcon } from './icons/CloseIcon'
 
-interface Props {}
-
 export const SideMenu = () => {
+  const router = useRouter()
   const { isMenuOpen, toggleSideMenu } = useUI()
-  const navigateTo = (url: string) => {
+  const [mounted, setMounted] = useState(false)
+  const [search, setSearch] = useState('')
+  const onSearch = () => {
+    if (search.length === 0) return
     toggleSideMenu()
+    router.push(`/search/${search}`)
   }
+
+  useEffect(() => {
+    if (window === undefined) return
+    setMounted(true)
+  }, [])
 
   return (
     <>
-      {isMenuOpen && (
-        <div className="fixed top-0 z-10 flex justify-end w-screen h-screen bg-black bg-opacity-60 backdrop-blur-sm">
-          <section className="relative h-full py-12 space-y-4 bg-white w-80">
-            <button className="absolute transition-transform top-4 left-4" onClick={toggleSideMenu}>
-              <CloseIcon />
-            </button>
-            <section className="relative px-8 group">
-              <input
-                className="w-full py-1 pl-2 border-b outline-none focus:border-black"
-                placeholder="..."
-                type="text"
+      {mounted &&
+        createPortal(
+          <>
+            {isMenuOpen && (
+              <div
+                className={`fixed top-0 z-10 flex justify-end w-screen h-screen bg-black bg-opacity-60 backdrop-blur-sm`}
               />
-              <button className="absolute outline-none right-8 bottom-2">
-                <SearchIcon className="w-5 h-5 text-gray-800 hover:text-black" />
-              </button>
-            </section>
-            <section className="w-5/6 py-2 mx-auto border-b">
-              <h4 className="px-4 mb-2 font-semibold bg-gray-100 rounded-full w-fit">Categorías</h4>
-              <ul>
-                <SideMenuItem
-                  href="/category/men"
-                  icon={<MaleIcon />}
-                  title="Hombres"
-                  onClick={() => navigateTo('/category/men')}
-                />
-                <SideMenuItem
-                  href="/category/women"
-                  icon={<FemaleIcon />}
-                  title="Mujeres"
-                  onClick={() => navigateTo('/category/woman')}
-                />
-                <SideMenuItem
-                  href="/category/kid"
-                  icon={<ChildrenIcon />}
-                  title="Niños"
-                  onClick={() => navigateTo('/category/kid')}
-                />
-              </ul>
-            </section>
-            <section className="w-5/6 py-2 mx-auto">
-              <ul>
-                <SideMenuItem href="/" icon={<UserIcon />} title="Perfil" />
-                <SideMenuItem href="/" icon={<OrdersIcon />} title="Mis Ordenes" />
-                <SideMenuItem href="/" icon={<SignUpIcon />} title="Ingresar" />
-                <SideMenuItem href="/" icon={<LogoutIcon />} title="Salir" />
-              </ul>
-            </section>
-          </section>
-        </div>
-      )}
+            )}
+            <div
+              className={`fixed top-0 right-0 ${isMenuOpen ? 'z-20' : '-z-10'} ${inter.className}`}
+            >
+              <section
+                className={`relative h-screen py-12 space-y-4 bg-white w-80 transition-transform duration-150 ${
+                  isMenuOpen ? 'translate-x-0' : 'translate-x-96'
+                }`}
+              >
+                <button
+                  className="absolute transition-transform top-4 left-4"
+                  onClick={toggleSideMenu}
+                >
+                  <CloseIcon />
+                </button>
+                <section className="relative px-8 group">
+                  <input
+                    autoFocus
+                    className="w-full py-1 pl-2 border-b outline-none focus:border-black"
+                    placeholder="..."
+                    type="text"
+                    value={search}
+                    onChange={(e) => setSearch(e.target.value)}
+                    onKeyUp={(e) => e.key === 'Enter' && onSearch()}
+                  />
+                  <button className="absolute outline-none right-8 bottom-2" onClick={onSearch}>
+                    <SearchIcon className="w-5 h-5 text-gray-800 hover:text-black" />
+                  </button>
+                </section>
+                <section className="w-5/6 py-2 mx-auto border-b">
+                  <h4 className="px-4 mb-2 font-semibold bg-gray-100 rounded-full w-fit">
+                    Categorías
+                  </h4>
+                  <ul>
+                    <SideMenuItem
+                      href="/category/men"
+                      icon={<MaleIcon />}
+                      title="Hombres"
+                      onClick={() => toggleSideMenu()}
+                    />
+                    <SideMenuItem
+                      href="/category/women"
+                      icon={<FemaleIcon />}
+                      title="Mujeres"
+                      onClick={() => toggleSideMenu()}
+                    />
+                    <SideMenuItem
+                      href="/category/kid"
+                      icon={<ChildrenIcon />}
+                      title="Niños"
+                      onClick={() => toggleSideMenu()}
+                    />
+                  </ul>
+                </section>
+                <section className="w-5/6 py-2 mx-auto">
+                  <ul>
+                    <SideMenuItem href="/" icon={<UserIcon />} title="Perfil" />
+                    <SideMenuItem href="/" icon={<OrdersIcon />} title="Mis Ordenes" />
+                    <SideMenuItem href="/" icon={<SignUpIcon />} title="Ingresar" />
+                    <SideMenuItem href="/" icon={<LogoutIcon />} title="Salir" />
+                  </ul>
+                </section>
+              </section>
+            </div>
+          </>,
+          document.body
+        )}
     </>
   )
 }
